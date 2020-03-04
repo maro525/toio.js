977911b6e484886101a0f9fbbb3a1c620e8c426c
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-03-04 15:39:08 +0900

    cubeを10個接続

--- examples/keyboard-control/index.js
+++ examples/keyboard-control/index.js
@@ -20,12 +20,30 @@ const SPEED = {
 async function main() {
   // start a scanner to find nearest cube
   // const cube = await new NearestScanner().start()
-  const cubes = await new NearScanner(2).start()
+  const cubes = await new NearScanner(10).start()
 
   // connect to the cube
   // await cube.connect()
-  const cube1 = await cubes[0].connect()
-  const cube2 = await cubes[1].connect()
+  const cube0 = await cubes[0].connect()
+  console.log("cube1");
+  const cube1 = await cubes[1].connect()
+  console.log("cube2");
+  const cube2 = await cubes[2].connect()
+  console.log("cube3");
+  const cube3 = await cubes[3].connect()
+  console.log("cube4");
+  const cube4 = await cubes[4].connect()
+  console.log("cube5");
+  const cube5 = await cubes[5].connect()
+  console.log("cube6");
+  // const cube6 = await cubes[6].connect()
+  console.log("cube7");
+  // const cube7 = await cubes[7].connect()
+  console.log("cube8");
+  // const cube8 = await cubes[8].connect()
+  console.log("cube9");
+  // const cube9 = await cubes[9].connect()
+  console.log("cube10");
 
   keypress(process.stdin)
   process.stdin.on('keypress', (ch, key) => {
@@ -37,13 +55,29 @@ async function main() {
     switch (key.name) {
       case 'up':
         // cube.move(...SPEED.forward, DURATION)
+        cube0.move(...SPEED.forward, DURATION)
         cube1.move(...SPEED.forward, DURATION)
         cube2.move(...SPEED.forward, DURATION)
+        cube3.move(...SPEED.forward, DURATION)
+        cube4.move(...SPEED.forward, DURATION)
+        cube5.move(...SPEED.forward, DURATION)
+        cube6.move(...SPEED.forward, DURATION)
+        cube7.move(...SPEED.forward, DURATION)
+        cube8.move(...SPEED.forward, DURATION)
+        cube9.move(...SPEED.forward, DURATION)
         break
       case 'down':
         // cube.move(...SPEED.backward, DURATION)
+        cube0.move(...SPEED.backward, DURATION)
         cube1.move(...SPEED.backward, DURATION)
         cube2.move(...SPEED.backward, DURATION)
+        cube3.move(...SPEED.backward, DURATION)
+        cube4.move(...SPEED.backward, DURATION)
+        cube5.move(...SPEED.backward, DURATION)
+        cube6.move(...SPEED.backward, DURATION)
+        cube7.move(...SPEED.backward, DURATION)
+        cube8.move(...SPEED.backward, DURATION)
+        cube9.move(...SPEED.backward, DURATION)
         break
       case 'left':
         // cube.move(...SPEED.left, DURATION)
